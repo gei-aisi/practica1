fd0beaff4a45aaa5ea1f0dbc791b4b84c8b017b6
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2024-01-18 17:12:52 +0100

    Update Docker installation for Ubuntu Jammy

--- provisioning/install-docker-ubuntu.sh
+++ provisioning/install-docker-ubuntu.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo -e "deb https://ftp.udc.es/ubuntu focal main restricted
-deb https://ftp.udc.es/ubuntu focal-updates main restricted
-deb https://ftp.udc.es/ubuntu focal-security main restricted
-deb https://ftp.udc.es/ubuntu focal universe
-deb https://ftp.udc.es/ubuntu focal-updates universe" | sudo tee /etc/apt/sources.list > /tmp/repo-log
+echo -e "deb https://ftp.udc.es/ubuntu jammy main restricted universe
+deb https://ftp.udc.es/ubuntu jammy-updates main restricted universe
+deb https://ftp.udc.es/ubuntu jammy-security main restricted" | sudo tee /etc/apt/sources.list > /tmp/repo-log
 
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get install -y ca-certificates curl lynx gnupg lsb-release
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo /usr/sbin/usermod -aG docker ${USER}
